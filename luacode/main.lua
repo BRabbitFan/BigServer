46059eb0a5ebe5cdc8eb0ffbbec0b998ff403476
BRabbitFan <fanjh1999@gmail.com>
2020-12-31 20:10:21 +0800

    更新入口服务中开启网关服务方法

--- luacode/main.lua
+++ luacode/main.lua
@@ -1,10 +1,29 @@
 local skynet = require "skynet"
 local sprotoloader = require "sprotoloader"
+local sharedata = require "sharedata"
 
-local max_client = 64
+local function startGate()
+	-- local watchdog = skynet.newservice("watchdog")
+	-- skynet.call(watchdog, "lua", "start", {
+	-- 	port = 8888,
+	-- 	maxclient = max_client,
+	-- 	nodelay = true,
+	-- })
+	local gateway_port = skynet.getenv("gateway_port") or 8000
+	local max_client = skynet.getenv("max_client") or 32
+	local watchdog = skynet.newservice("watchdog")
+	skynet.call(watchdog, "lua", "start", {
+		port = gateway_port,
+		maxclient = max_client,
+		nodelay = true,
+		servername = "gate",
+	})
+	sharedata.new("gate", watchdog)
+end
 
 skynet.start(function()
-	skynet.error("hello world")
+	skynet.error("Big Start!")
+	startGate()
 	-- skynet.error("Server start")
 	-- skynet.uniqueservice("protoloader")
 	-- if not skynet.getenv "daemon" then
@@ -12,12 +31,7 @@ skynet.start(function()
 	-- end
 	-- skynet.newservice("debug_console",8000)
 	-- skynet.newservice("simpledb")
-	-- local watchdog = skynet.newservice("watchdog")
-	-- skynet.call(watchdog, "lua", "start", {
-	-- 	port = 8888,
-	-- 	maxclient = max_client,
-	-- 	nodelay = true,
-	-- })
+
 	-- skynet.error("Watchdog listen on", 8888)
 	skynet.exit()
 end)
